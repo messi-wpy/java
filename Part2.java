8dddcc7b191ee55836b9d5913bfe3700a0ebc994
messi-wpy <904315105@qq.com>
2017-11-28 11:36:11 +0800

    wpy

---
+++ Part2.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Part2 {
+    public static void main(String[] args)
+    {
+        int a[]={1,2,3,4,5,6,7,8,9};
+        returna(a);
+    }
+    public static void returna(int x[])
+    {
+        System.out.println(Arrays.toString(x));
+
+    }
+}
